4ec607f82c82ef16baf1d744eb1147ced0dc5729
costin88boss <42942503+costin88boss@users.noreply.github.com>
2021-01-22 21:22:27 +0200

    Add files via upload

---
+++ Commands/Listbans.cs
@@ -0,0 +1,48 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Easy_Unban
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    internal class Listbans : ICommand
+    {
+
+            
+        public string[] Aliases { get; } = plugin.Singleton.Config.ListBansCmdAliases;
+
+        public string Description { get; } = plugin.Singleton.Config.ListBansCmdInfo;
+
+        string ICommand.Command { get; } = plugin.Singleton.Config.ListBansCmd;
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = plugin.Singleton.Config.ListBansCmdShowingListMsg;
+            if (plugin.IdBanlist != null)
+            {
+                response += "\n" + plugin.Singleton.Config.ListBansCmdShowingIdList;
+                for (int i = 0; i < plugin.IdBanlist.Count; i++)
+                {
+                    string message = plugin.Singleton.Config.ListBansCmdBanList.Replace("{BanNumber}", plugin.IdBanlist[i].Id.ToString()).Replace("{PlayerName}", plugin.IdBanlist[i].Name);
+                    response += "\n" + message;
+                }
+            }
+            else response += "\n" + plugin.Singleton.Config.NoIpORIdbannedUserFound.Replace("{BanType}", "UserId");
+            if (plugin.IpBanlist != null)
+            {
+                response += "\n---Ip banuri---";
+                for (int i = 0; i < plugin.IpBanlist.Count; i++)
+                {
+                    string message = plugin.Singleton.Config.ListBansCmdBanList.Replace("{BanNumber}", plugin.IpBanlist[i].Id.ToString()).Replace("{PlayerName}", plugin.IpBanlist[i].Name);
+                    response += "\n" + message;
+                }
+            }
+            else response += "\n" + plugin.Singleton.Config.NoIpORIdbannedUserFound.Replace("{BanType}", "Ip");
+
+            return true;
+
+        }
+    }
+}
